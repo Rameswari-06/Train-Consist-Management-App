0e5ebe0aef82b961df7ac308222670b2566b68cd
Rameswari-06 <rameswaribarick@gmail.com>
2026-03-25 14:55:05 +0530

    UC3

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TrainConsistManagementApp {
 
@@ -32,5 +34,16 @@ public class TrainConsistManagementApp {
 
         System.out.println("\nFinal passenger bogie list:");
         System.out.println(passengerBogies);
+
+        Set<String> bogieIds = new HashSet<>();
+
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG101");
+        bogieIds.add("BG103");
+        bogieIds.add("BG102");
+
+        System.out.println("\nUnique Bogie IDs:");
+        System.out.println(bogieIds);
     }
 }
